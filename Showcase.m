dbe8c7a274b0bc9659dc8876a0b916c6f3b47722
dandub03 <72382210+dandub03@users.noreply.github.com>
2023-05-19 23:47:10 +0200

    Update Showcase.m

--- Showcase.m
+++ Showcase.m
@@ -1,72 +1,85 @@
-% 3.1
-% a)
-Fs = 1000;                   % samples per second
-dt = 1/Fs;                   % seconds per sample
-N = 1000
-i = (0:N-1)
-Ts = 1/Fs
-time = i * Ts
-%%Sine wave:
-Fc = 50;                     % frequency
-A = 1;                       % amplitude
-x = A*sin(2*pi*Fc*time) % sine wave 
-sq = [ones(1,Fs/Fc/2), -1*ones(1,Fs/Fc/2)]; % single square wave period
-xsquare = A*repmat(sq,1,N/length(sq)); % square wave
-xrand = randn(N,1); % random signal with mean value 0 and standard deviation 1
+clear 
+rng('default');
+load("exampleMaps.mat");
+map = occupancyMap(complexMap);
+% map = mapClutter(30, ["Box", "Circle"],'MapSize', [100 100], 'MapResolution', 1);
+planner = plannerAStarGrid(map);
+% show(planner)
 
-% b)
-% Sine signal plot
-figure;
-plot(time(1:100),x(1:100));
-xlabel('time');
-title('Sine signal');
-zoom xon;
-% Square signal plot
-figure;
-plot(time(1:100),xsquare(1:100));
-xlabel('time');
-title('Square signal');
-zoom xon;
-% Random signal plot
-figure;
-plot(time(1:100),xrand(1:100));
-xlabel('time');
-title('Square signal');
-zoom xon;
+start = [39 3];
+koniec = [17 38];
 
-% c)
-% Sine signal
-X_fft = Ts*fft(x,N);
-y = (abs(X_fft).^2)/N/Ts;
-X_psd = y(1:N/2);
-X_psd_dB = 10*log10(X_psd);
-freq = (0:N/2-1)*Fs/N;
-figure;
-plot(freq,X_psd_dB,'-*')
-xlabel('Frequency');
-ylabel('dB');
-% Square signal
-X_fft = Ts*fft(xsquare,N);
-y = (abs(X_fft).^2)/N/Ts;
-X_psd = y(1:N/2);
-X_psd_dB = 10*log10(X_psd);
-freq = (0:N/2-1)*Fs/N;
-figure;
-plot(freq,X_psd_dB,'-*');
-xlabel('Frequency');
-ylabel('dB');
-% Random signal
-X_fft = Ts*fft(xrand,N);
-y = (abs(X_fft).^2)/N/Ts;
-X_psd = y(1:N/2);
-X_psd_dB = 10*log10(X_psd);
-freq = (0:N/2-1)*Fs/N;
-figure;
-plot(freq,X_psd_dB,'-*');
-xlabel('Frequency');
-ylabel('dB');
-% 3.2
+sciezka = plan(planner,start,koniec); %Y,X
+skoki = diff(sciezka);
+koszt = 0;
+krok = 1;
+idz = 1;
+theta = 0;
+pozycja = sciezka(krok,:); %ustawienie pozycji
 
-% 3.3
+%kalkulacja kosztow
+for i = 1:size(skoki,1)
+    koszt = koszt + norm(skoki(i,:));
+end
+disp(koszt)
 
-% 3.4
+% initialize figure and plot map and path
+figure;
+% show(planner);
+% hold on;
+% plot(sciezka(:,2), sciezka(:,1), 'r', 'LineWidth', 2);
+
+% simulate robot movement
+robotSize = 0.5; % size of the robot (square side length)
+fluidTurnTime = 0.5; % time to simulate a fluid turn (in seconds)
+isgoal = false;
+while isgoal == false
+    show(planner);
+    hold on;
+    plot(sciezka(:,2), sciezka(:,1), 'y', 'LineWidth', 2);
+    % update position and angle
+    pozycja = sciezka(krok,:);
+    if idz == 1
+        krok = krok + 1;
+        if krok <= size(sciezka, 1)
+            skok = sciezka(krok,:) - sciezka(krok-1,:);
+            targetTheta = atan2(skok(1), skok(2));
+            deltaTheta = targetTheta - theta;
+            fluidTurnSteps = fluidTurnTime / 0.1; % divide the time into steps
+            turnIncrement = deltaTheta / fluidTurnSteps;
+            for step = 1:fluidTurnSteps
+                theta = theta + turnIncrement;
+                % plot robot position
+                robotX = pozycja(2);
+                robotY = pozycja(1);
+                plotSquare(robotX, robotY, robotSize, theta);
+                % pause to visualize the movement
+                pause(0.1);
+            end
+        end
+    end
+    
+    % plot robot position
+    robotX = pozycja(2);
+    robotY = pozycja(1);
+    plotSquare(robotX, robotY, robotSize, theta);
+    
+    if robotX == koniec(2) && robotY == koniec(1);
+        isgoal = true;
+    end
+    
+    if idz == 0
+    break
+    end
+    
+    % pause to visualize the movement
+    pause(0.1);
+end
+
+% function to plot a square representing the robot
+function plotSquare(x, y, size, angle)
+    R = [cos(angle) -sin(angle); sin(angle) cos(angle)]; % rotation matrix
+    square = [-1 -1; -1 1; 1 1; 1 -1]; % square vertices
+    transformedSquare = (square * R') * size + [x y];
+    fill(transformedSquare(:,1), transformedSquare(:,2), 'b');
+end
